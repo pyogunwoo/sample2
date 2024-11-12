0bcb4a4571b27cda4d64e53f2d6839b0d23cbcd7
kosmo <vyrjsdn5038@naver.com>
2024-11-12 10:20:56 +0900

    두번째 커밋 메시지

--- src/main/java/com/kosmo/ex/Main.java
+++ src/main/java/com/kosmo/ex/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
         System.out.println("보이냐");
+        System.out.println("보이냐222222");
 
     }
 }
